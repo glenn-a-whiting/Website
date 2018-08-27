0f17b3aa518bdc5e80dfc042ba4ad7def7a594bb
glenn-a-whiting <glenn.a.whiting@gmail.com>
2018-08-27 16:54:17 +1000

    Corrected script source link

--- resources/views/page/html/Website_Layout_Designer/index.blade.php
+++ resources/views/page/html/Website_Layout_Designer/index.blade.php
@@ -1,7 +1,7 @@
 <html onclick="mousePressed(event)" onmousemove="mouseMoved(event)">
 	<head>
 		<title>Bootstrap Layouter</title>
-		<script src="{{('js/Website_Layout_Designer/script.js')}}"></script>
+		<script src="{{asset('js/Website_Layout_Designer/script.js')}}"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
