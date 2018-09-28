2885c3496cad811882175af901ad6787c99a3d89
glenn-a-whiting <glenn.a.whiting@gmail.com>
2018-09-28 19:58:29 +1000

    Added aliasing function to PageController

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -7,6 +7,15 @@ use App\Page;
 
 class PageController extends Controller
 {
+	static private $ALIASES = [
+		"game" => "Survival_Game"
+	];
+
+	static private function alias($a){
+		if(in_array($a,array_keys(self::$ALIASES)))return self::$ALIASES[$a];
+		else return $a;
+	}
+
     public function index(){
 		return view("home",[
 			"pages" => Page::PAGES
@@ -14,7 +23,8 @@ class PageController extends Controller
 	}
 
 	public function page($page){
-		return view("page.".$page.".index",Page::PAGES[$page])->with("page",$page);
+		$alias = self::alias($page);
+		return view("page.".$alias.".index",Page::PAGES[$alias])->with("page",$alias);
 	}
 
 	public function subdirectory($page,$directory){
